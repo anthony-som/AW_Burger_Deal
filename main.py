afea9c8035064b075c80feb75aca128730c9b3ee
anthony <qiko0425@gmail.com>
2024-03-02 00:47:19 -0500

    fixed date formatting

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ def job():
     def game_check():
         if json_data and 'games' in json_data:
             for game in json_data['games']:
-                game_date = game['gameDate']
+                game_date = datetime.strptime(game['gameDate'], '%Y-%m-%d').date()
                 if game_date == current_date:
                     return True
                 else:
                     return False
 
     game_today = game_check()
+
     
     if game_today:
         toaster.show_toast('A&W Burger', 'There is a $2 Teen burger on the A&W App, go get it!', duration=10)
-        print('There is a game today')
     else:
         toaster.show_toast('A&W Burger', 'There is no teen burger deal today! ', duration=10)
-        print('There is no game today')
+        
 job() # run job once on startup
 
 # Schedule the job every minute
